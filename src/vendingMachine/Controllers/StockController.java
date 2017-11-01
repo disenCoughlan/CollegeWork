f036f51a249bb5dc50e7ce293b817063ae5a1896
disenCoughlan <disen.nano@hotmail.com>
2017-11-01 14:35:16 +0000

    added a view of stock in the stock page
    i dont know if i am doing it right

--- src/vendingMachine/Controllers/StockController.java
+++ src/vendingMachine/Controllers/StockController.java
@@ -1,12 +1,19 @@
 package vendingMachine.Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import vendingMachine.Managers.SceneManager;
 
 public class StockController {
 
+    public Label listedItem0;
+    public Label listedItem1;
+
     public void openVendingMachine(ActionEvent actionEvent) throws Exception
     {
         SceneManager.SwitchToScene("VendingMachine");
+    }
+
+    public void listedItem(ActionEvent actionEvent) {
     }
 }
