2360bd81694c7f3e621014000cdde5c3e84a8ace
disenCoughlan <disen.nano@hotmail.com>
2017-11-02 23:47:40 +0000

    updated stock controller and view

--- src/vendingMachine/Controllers/StockController.java
+++ src/vendingMachine/Controllers/StockController.java
@@ -2,27 +2,56 @@ package vendingMachine.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import vendingMachine.Managers.SceneManager;
 
 import java.math.BigDecimal;
+import java.util.regex.Pattern;
 
 public class StockController {
 
-    public Label listedItem0;
-    public Label listedItem1;
-    public Label balanceOfMachine;
-    private BigDecimal bal = new  BigDecimal (0.00);
+
+    public TextField productLocationField;
+    public TextField productNameField;
+    public TextField productPriceField;
+    public Label validationErrorLabel;
+    public Label SuccessNotification;
 
     public void openVendingMachine(ActionEvent actionEvent) throws Exception
     {
         SceneManager.SwitchToScene("VendingMachine");
     }
 
-    public void listedItem(ActionEvent actionEvent) {
+    public void addProduct(ActionEvent actionEvent) {
+        SuccessNotification.setText("");
+        validationErrorLabel.setText("");
+        StringBuilder errorMessages = new StringBuilder();
+        try {
+            int productLocation = getProductLocation();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            errorMessages.append(e.getMessage() + "\n");
+        }
+        String productName = "";
+        BigDecimal productPrice;
+
+        validationErrorLabel.setText(errorMessages.toString());
+        if(errorMessages.toString() != null  || errorMessages.toString().length() > 0)
+            SuccessNotification.setText("Product Added");
     }
 
-    public void getBal(){
-       String A = balanceOfMachine.getText();
-
+    public int getProductLocation() throws Exception {
+        if(productLocationField.getText() != null && productLocationField.getText() != "" && productLocationField.getText().length() == 2 )
+        {
+            try{
+                boolean hasMatch = Pattern.matches("^[0-4][0-2]$", productLocationField.getText());
+                if(hasMatch )
+                    return Integer.parseInt(productLocationField.getText());
+            }catch (Exception e){
+                throw new Exception("Product location is not valid number");
+            }
+        }
+        throw new Exception("Product location is not valid");
     }
+    //(^[0-9]?\.?[0-9]?[0-9]$) pattern for price
 }
