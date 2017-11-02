9ccd924c2e1be80f56f867f45b628df70db34cf2
disenCoughlan <disen.nano@hotmail.com>
2017-11-02 15:36:52 +0000

    i dont know why this is being called an error

--- src/vendingMachine/Controllers/StockController.java
+++ src/vendingMachine/Controllers/StockController.java
@@ -4,10 +4,14 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 import vendingMachine.Managers.SceneManager;
 
+import java.math.BigDecimal;
+
 public class StockController {
 
     public Label listedItem0;
     public Label listedItem1;
+    public Label balanceOfMachine;
+    private BigDecimal bal = new  BigDecimal (0.00);
 
     public void openVendingMachine(ActionEvent actionEvent) throws Exception
     {
@@ -15,5 +19,10 @@ public class StockController {
     }
 
     public void listedItem(ActionEvent actionEvent) {
+    }
+
+    public void getBal(){
+       String A = balanceOfMachine.getText();
+
     }
 }
